686d4777c4805e366fc4056640a49b2981f01c82
Gayathri <gaaya3cg@gmail.com>
2022-10-13 19:14:51 -0400

    updated DB

--- server/index.js
+++ server/index.js
@@ -317,11 +317,15 @@ const uploadProductImages = upload.fields([
 app.post("/products", uploadProductImages, resizerImages, createProduct);
 
 const db = mysql.createConnection({
-    host: '3.87.8.163',
-    user: 'admin',
+    // host: '3.87.8.163',
+    // user: 'admin',
+    // password: 'Gaya5544',
+    // port: 3306,
+    // database: 'pikrush'
+    host:'database-1.c4faqpjzo0qu.us-east-1.rds.amazonaws.com',
+    user:'admin',
     password: 'Gaya5544',
-    port: 3306,
-    database: 'pikrush'
+    database:'pikrush'
 
 });
 
